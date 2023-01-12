1cf820b88b53f59052dc1927e9aa4eae89cf2229
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2023-01-12 09:17:10 -0300

    Algoritmo de ordenação mergesort

---
+++ JavaScript/mergeSort.js
@@ -0,0 +1,33 @@
+function merge(left, right){
+    let sortedArr = []
+    /* Enquanto um dos dois arrays tiverem elementos */
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            sortedArr.push(left.shift())
+        } else {
+            sortedArr.push(right.shift())
+        }
+    }
+    return [...sortedArr, ...left, ...right]
+}
+
+function mergeSort(array) {
+    /* Caso base */
+    if(array.length <= 1){
+        return array
+    }
+    
+    /* Achar o meio do array */
+    let center = Math.floor(array.length / 2);
+
+    /* Dividir o array na metade e mandar para função novamente */
+    let left = mergeSort(array.slice(0, center));
+    let right = mergeSort(array.slice(center));
+    
+    /* Faz o merge */
+    return merge(left, right);
+}
+
+console.log(
+    mergeSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92])
+)
